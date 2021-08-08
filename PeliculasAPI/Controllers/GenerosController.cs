ec2fd1f27bbc2dfc9bac9d8b9c4bd7e49bb72c59
jrantul <jrantul@yahoo.com>
2021-08-07 22:14:17 -0400

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GenerosController(ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GeneroDTO>>> Get()
+        {
+            var entidades = await context.Generos.ToListAsync();
+            var dtos = mapper.Map<List<GeneroDTO>>(entidades);
+            return dtos;
+        }
+    }
+}
